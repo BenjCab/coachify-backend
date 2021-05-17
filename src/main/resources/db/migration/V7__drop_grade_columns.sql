0fe10c7a02770c0d84e10a4f71e01d2a1eead078
bchaerel <brecht.chaerel@gmail.com>
2021-05-17 10:37:07 +0200

    extra: dropped the extra columns

---
+++ src/main/resources/db/migration/V7__drop_grade_columns.sql
@@ -0,0 +1,8 @@
+alter table topic_by_coach drop column grade4;
+
+alter table topic_by_coach drop column grade5;
+
+alter table topic_by_coach drop column grade6;
+
+alter table topic_by_coach drop column grade7;
+
