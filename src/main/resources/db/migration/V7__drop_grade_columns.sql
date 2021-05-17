47d0336aefb79f0060188da4a675b0b68831e6e8
bchaerel <brecht.chaerel@gmail.com>
2021-05-17 10:43:24 +0200

    extra: altered the V7 flyway + deleted the surefire dependency

--- src/main/resources/db/migration/V7__drop_grade_columns.sql
+++ src/main/resources/db/migration/V7__drop_grade_columns.sql
@@ -1,8 +1,8 @@
-alter table topic_by_coach drop column grade4;
+alter table topic_by_coach drop column if exists grade4;
 
-alter table topic_by_coach drop column grade5;
+alter table topic_by_coach drop column if exists grade5;
 
-alter table topic_by_coach drop column grade6;
+alter table topic_by_coach drop column if exists grade6;
 
-alter table topic_by_coach drop column grade7;
+alter table topic_by_coach drop column if exists grade7;
 
