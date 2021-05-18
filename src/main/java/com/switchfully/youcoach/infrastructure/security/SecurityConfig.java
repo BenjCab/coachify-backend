ca326003fa7da3c0c6203f00bee980f9c51d53ce
Robert Khomiakov <robertkhomiakov@hotmail.com>
2021-05-18 16:59:11 +0200

    changed profile of method

--- src/main/java/com/switchfully/youcoach/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/switchfully/youcoach/infrastructure/security/SecurityConfig.java
@@ -24,7 +24,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-@Profile("development")
+@Profile("production")
 @EnableWebSecurity(debug = false)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
