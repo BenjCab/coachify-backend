af286466ad836d184523cea25bd0ed066c28acff
bchaerel <brecht.chaerel@gmail.com>
2021-05-18 16:40:18 +0200

    changed production into development in the securityconfig and in the application.properties

--- src/main/java/com/switchfully/youcoach/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/switchfully/youcoach/infrastructure/security/SecurityConfig.java
@@ -23,7 +23,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-@Profile("production")
+@Profile("development")
 @EnableWebSecurity(debug = false)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
