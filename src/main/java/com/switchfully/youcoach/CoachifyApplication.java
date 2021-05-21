684c5438d68dd7ec8e95b27d1db736c3fea5e637
bchaerel <brecht.chaerel@gmail.com>
2021-05-21 14:31:43 +0200

    deleted some dependencies and renamed the app name

---
+++ src/main/java/com/switchfully/youcoach/CoachifyApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.youcoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoachifyApplication.class, args);
+    }
+
+}
