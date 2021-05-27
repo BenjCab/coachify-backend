4f746df2f46917d2ed5c042aade1cff3ad866473
ben <benjamin.cabaux@gmail.com>
2021-05-27 13:58:24 +0200

    adding stuff to the database with the V7

--- src/main/resources/db/migration/V7__adding_some_coaches.sql
+++ src/main/resources/db/migration/V7__adding_some_coaches.sql
@@ -3,30 +3,30 @@ set schema 'public';
 insert into accounts(first_name, last_name, email, password, enabled)
 values ('Bob', 'TheBuilder', 'bob@test.com', '$2a$10$fMwGqxsxtRttQsFl3g8GXeYDK3BJMeZH4MM9ZYlgnw9T42cxXux7u', true);
 insert into authorities(account_id, authority)
-values (10, 'COACH');
+values (4, 'COACH');
 insert into authorities(account_id, authority)
-values (10, 'COACHEE');
+values (4, 'COACHEE');
 
 insert into accounts(first_name, last_name, email, password, enabled)
 values ('Gerald', 'Bowl', 'gerald@test.com', '$2a$10$fMwGqxsxtRttQsFl3g8GXeYDK3BJMeZH4MM9ZYlgnw9T42cxXux7u', true);
 insert into authorities(account_id, authority)
-values (11, 'COACH');
+values (5, 'COACH');
 insert into authorities(account_id, authority)
-values (11, 'COACHEE');
+values (5, 'COACHEE');
 
 insert into accounts(first_name, last_name, email, password, enabled)
 values ('Rick', 'Sanchez', 'rick@test.com', '$2a$10$fMwGqxsxtRttQsFl3g8GXeYDK3BJMeZH4MM9ZYlgnw9T42cxXux7u', true);
 insert into authorities(account_id, authority)
-values (12, 'COACH');
+values (6, 'COACH');
 insert into authorities(account_id, authority)
-values (12, 'COACHEE');
+values (6, 'COACHEE');
 
 insert into accounts(first_name, last_name, email, password, enabled)
 values ('TheDoctor', 'Doctor', 'doctor@test.com', '$2a$10$fMwGqxsxtRttQsFl3g8GXeYDK3BJMeZH4MM9ZYlgnw9T42cxXux7u', true);
 insert into authorities(account_id, authority)
-values (13, 'COACH');
+values (7, 'COACH');
 insert into authorities(account_id, authority)
-values (13, 'COACHEE');
+values (7, 'COACHEE');
 
 insert into topics(topic_name) values ('C#');
 insert into topics(topic_name) values ('C++');
@@ -34,33 +34,32 @@ insert into topics(topic_name) values ('PHP');
 insert into topics(topic_name) values ('Python-Turtle');
 
 insert into coach_profile(account_id, introduction, availability, image)
-values (10, 'intro', 'every other day', './assets/images/profile-picture-coach.png');
+values (4, 'intro', 'every other day', './assets/images/profile-picture-coach.png');
 
 insert into coach_profile(account_id, introduction, availability, image)
-values (11, 'test intro', 'every monday', './assets/images/profile-picture-coach.png');
+values (5, 'test intro', 'every monday', './assets/images/profile-picture-coach.png');
 
 insert into coach_profile(account_id, introduction, availability, image)
-values (12, 'This is not an easter egg.', '2 first sundays of the month', './assets/images/profile-picture-coach.png');
+values (6, 'This is not an easter egg.', '2 first sundays of the month', './assets/images/profile-picture-coach.png');
 
 insert into coach_profile(account_id, introduction, availability, image)
-values (13, 'This is an introduction. im a human, not a robot. there is no planned robot invasion.', 'idk just ask me', './assets/images/profile-picture-coach.png');
+values (7, 'This is an introduction. im a human, not a robot. there is no planned robot invasion.', 'idk just ask me', './assets/images/profile-picture-coach.png');
 
 insert into topic_by_coach(coach_profile_id, topic_id, grade1, grade2, grade3)
-values(10, 3, true, false, false);
+values(4, 3, true, false, false);
 insert into topic_by_coach(coach_profile_id, topic_id, grade1, grade2, grade3)
-values(10, 2, true, false, false);
+values(4, 2, true, false, false);
 insert into topic_by_coach(coach_profile_id, topic_id, grade1, grade2, grade3)
-values(10, 5, true, false, false);
+values(4, 5, true, false, false);
 insert into topic_by_coach(coach_profile_id, topic_id, grade1, grade2, grade3)
-values(11, 6, true, false, false);
+values(5, 6, true, false, false);
 insert into topic_by_coach(coach_profile_id, topic_id, grade1, grade2, grade3)
-values(11, 5, true, false, false);
+values(5, 5, true, false, false);
 insert into topic_by_coach(coach_profile_id, topic_id, grade1, grade2, grade3)
-values(12, 5, true, false, false);
+values(6, 5, true, false, false);
 insert into topic_by_coach(coach_profile_id, topic_id, grade1, grade2, grade3)
-values(12, 4, true, false, false);
+values(6, 4, true, false, false);
 insert into topic_by_coach(coach_profile_id, topic_id, grade1, grade2, grade3)
-values(13, 2, true, false, false);
+values(7, 2, true, false, false);
 insert into topic_by_coach(coach_profile_id, topic_id, grade1, grade2, grade3)
-values(13, 6, true, false, false);
-insert into topic_by_coach(coach_profile_id, topic_id, grade1, grade2, grade3)
+values(7, 6, true, false, false);
