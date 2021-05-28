fc957379100914089b002c6a32104573ad53d013
ben <benjamin.cabaux@gmail.com>
2021-05-28 09:55:51 +0200

    email to lowercase when reseting password

--- src/main/java/com/switchfully/youcoach/infrastructure/validations/ValidationUtil.java
+++ src/main/java/com/switchfully/youcoach/infrastructure/validations/ValidationUtil.java
@@ -27,4 +27,13 @@ final public class ValidationUtil {
         else if (isEmptyString(object))
             throw new IllegalArgumentException(objectName + " cannot be empty.");
     }
+
+    public static boolean isEmailValid(String email) {
+        if (email == null) return false;
+
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+    }
 }
