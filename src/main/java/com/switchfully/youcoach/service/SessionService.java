bf9cc94bdc18ff8d6109c1fc8ffbeb5ffa332f1e
pelinyvg <pelin.yuzbasi@gmail.com>
2021-05-31 15:43:41 +0200

    removes import

--- src/main/java/com/switchfully/youcoach/service/SessionService.java
+++ src/main/java/com/switchfully/youcoach/service/SessionService.java
@@ -13,8 +13,6 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
