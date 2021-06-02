acd3244afcbe08213c824dd1a7cde62c01785475
ben <benjamin.cabaux@gmail.com>
2021-06-02 11:53:31 +0200

    story 7

---
+++ src/main/java/com/switchfully/youcoach/api/DTOs/UpdateCoachProfileDTO.java
@@ -0,0 +1,24 @@
+package com.switchfully.youcoach.api.DTOs;
+
+public class UpdateCoachProfileDTO {
+    private String introduction;
+    private String availability;
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public UpdateCoachProfileDTO setIntroduction(String introduction) {
+        this.introduction = introduction;
+        return this;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public UpdateCoachProfileDTO setAvailability(String availability) {
+        this.availability = availability;
+        return this;
+    }
+}
